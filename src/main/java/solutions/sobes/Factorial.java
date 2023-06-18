b39735294993418b3801dbe5118716eaa80df558
777 <goszakupki.aktiv@gmail.com>
2023-06-18 19:06:23 +0300

    factorial ready

--- src/main/java/solutions/sobes/Factorial.java
+++ src/main/java/solutions/sobes/Factorial.java
@@ -2,6 +2,31 @@ package solutions.sobes;
 
 public class Factorial {
 
+    public static void main(String[] args) {
 
-    
+        int x = 5;
+
+        System.out.println(fac(x));
+        System.out.println(fucCicle(x));
+    }
+
+    private static int fucCicle(int x) {
+        if (x == 0) return 1;
+
+        int result = 1;
+        for (int i = 1; i <=x ; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    private static int fac(int x) {
+
+        if (x == 0) return 1;
+
+        return x * fac(x - 1);
+
+    }
+
+
 }
