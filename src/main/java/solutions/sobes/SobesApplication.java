dc65b771315597892888f2aa76b9ce6e1ae0035b
777 <goszakupki.aktiv@gmail.com>
2023-06-18 18:42:18 +0300

    sobes start

---
+++ src/main/java/solutions/sobes/SobesApplication.java
@@ -0,0 +1,13 @@
+package solutions.sobes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SobesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/solutions/sobes/SobesApplicationTests.java
@@ -0,0 +1,13 @@
+package solutions.sobes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
